6e2250549c8954453645d25afa33560bec5a379e
Mehmet Hüseyin Delipalta <mehmethuseyindelipalta@gmail.com>
2022-10-16 23:17:57 +0300

    Film listeleme yapısı oluşturuldu.

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,57 @@
 import React from "react";
+import ResultCard from "./ResultCard";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [query, setQuery] = React.useState("");
+  const [results, setResults] = React.useState([]);
+  function onChange(e) {
+    e.preventDefault();
+    setQuery(e.target.value);
+    //console.log(e.target.value);
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=false`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      });
+  }
+  return (
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <img src="https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)/7VrRna8S3x6xbijooeBmxqvHXiu.jpg"></img>
+          <div className="titles">
+            <h1>Hoş Geldiniz.</h1>
+            <h2>
+              Milyonlarca film, TV şovu ve keşfedilecek kişi. Şimdi keşfedin.
+            </h2>
+          </div>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              value={query}
+              onChange={onChange}
+              placeholder="Film, TV şovu veya kişi arayın"
+            />
+          </div>
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((movie) => (
+                <li key={movie.id}>
+                  <ResultCard movie={movie} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
