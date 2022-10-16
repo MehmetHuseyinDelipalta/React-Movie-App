eb3ac55af374bc370304072414145684432f3d6b
Mehmet Hüseyin Delipalta <mehmethuseyindelipalta@gmail.com>
2022-10-16 20:47:36 +0300

    Projenin ana yapısı oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./lib/fontawesome/css/all.min.css";
 import Header from "./components/Header";
+import Watched from "./components/Watched";
+import Watchlist from "./components/Watchlist";
+import Add from "./components/Add";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Routes>
+        <Route path="/" element={<Watchlist />} />
+        <Route path="/watched" element={<Watched />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
         <div className="inner-content">
-          <div className="brand">İzlenecekler</div>
+          <div className="brand">
+            <Link to={"/"}>İzlenecekler</Link>
+          </div>
           <ul className="nav-links">
-            <li>İzlenenler</li>
             <li>
-              <i className="fas fa-plus"></i>
+              <Link to={"/watched"}>İzlenenler</Link>
+            </li>
+            <li>
+              <Link to={"/add"} className="btn">
+                <i className="fas fa-plus"></i>
+              </Link>
             </li>
           </ul>
         </div>
