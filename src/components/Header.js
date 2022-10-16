af7818d88cb728a3a18bb2df7992be80ae0bb85c
Mehmet Hüseyin Delipalta <mehmethuseyindelipalta@gmail.com>
2022-10-16 20:00:03 +0300

    Header oluşturuldu.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">İzlenecekler</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
