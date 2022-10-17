b1ef8a9583dbd8b080a6c62c45bd417e4e1c80f6
Mehmet Hüseyin Delipalta <mehmethuseyindelipalta@gmail.com>
2022-10-17 14:21:57 +0300

    Local Storage yapısı eklendi.

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Watchlist = () => {
-  return <div>Watchlist</div>;
+  const { watchlist } = React.useContext(GlobalContext);
+  return (
+    <div>
+      {watchlist.map((movie) => (
+        <h1>{movie.title}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default Watchlist;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,23 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 //! create context
 export const GlobalContext = createContext();
 const initialState = {
-  watchlist: [],
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
   watched: [],
 };
 //! provider components
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //! actions
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+  }, [state]);
+
   const addMovieToWatchlist = (movie) => {
     dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
   };
